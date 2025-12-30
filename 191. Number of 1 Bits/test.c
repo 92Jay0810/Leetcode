3edc07f4f1a09b3edf868ad2eee0a73c21f9fb00
Hung Yi Jie <ggfdagwshghxfg@gmail.com>
2025-12-30 10:58:19 +0800

    add 338. Counting Bits

--- 191. Number of 1 Bits/test.c
+++ 191. Number of 1 Bits/test.c
@@ -4,7 +4,7 @@ int hammingWeight(int n) {
 
     for (int i = 0; i < 32; i++) {
         // 1u 很重要  1u 是 unsigned int
-        // 若把 1 看是 signed int 左移到第 31 bit → undefined behavior
+        // 若把 1 看是 signed int 左移到第 31 bit (sign bit) → undefined behavior
         if ((1u << i) & num) {
             count++;
         }
---
+++ 338. Counting Bits/dp.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        // 輸入 n=4 要check 0~4
+        vector<int> dp(n + 1);
+        // offset 作為最高2進制位元 i = 8 offset為8 i=7 offset為4
+        int offset = 1 ;
+        for(int i = 1 ; i <= n ; i++){
+            if(offset * 2 == i){
+                offset = i ;
+            }
+            // offset的bit算進來後 再拿前面算過的 7拿掉4之後 再拿3的結果
+            dp[i] = 1 + dp[i - offset];
+        }
+        return dp;
+    }
+};
